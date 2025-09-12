7b917be86246a186f854e29d21fc828ae171f90f
Sushant Dasuri <sushant.dasuri@oliver.agency>
2025-09-12 20:22:52 +0530

    Getting the Data

--- vite_sandbox/src/App.jsx
+++ vite_sandbox/src/App.jsx
@@ -1,7 +1,14 @@
-
+import { useEffect } from 'react'
 import './App.css'
 
 function App() {
+
+  useEffect(() => {
+    let results = {};
+    fetch('api/data.json', {headers: { 'Content-Type': 'application/json' }})
+      .then (res => res.json())
+      .then(data => console.log(data));
+  }, [])
 
   return (
     <>
